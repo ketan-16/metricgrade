803e79af393da88b77049d84aaf687139600b629
Pranil Kharche <pranilkharche@gmail.com>
2021-06-04 19:35:17 +0530

    Cors

--- rest_api/api.py
+++ rest_api/api.py
@@ -1,47 +1,21 @@
 import flask
 from flask import request, jsonify
 from questions import python
-import random
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-# A route to return all of the available entries in our catalog.
 @app.route('/api/questions/python', methods=['GET'])
 def api_all():
     args = request.args
     total_questions = int(args['totalquestions'])
-    first2pairs = {k: python[k]
-                   for k in sorted(python.keys())[:total_questions]}
-    print(first2pairs)
-    return jsonify(first2pairs)
+    question_set = {k: python[k]
+                    for k in sorted(python.keys())[:total_questions]}
+    print(question_set)
+    return jsonify(question_set), 200
 
 
 app.run()
